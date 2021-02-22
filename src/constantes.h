eb9bd647c68acd11ce8f90b478a9ab0e464dff26
Lucas Bouillon <lucasbouillon72@gmail.com>
2021-02-22 14:42:35 +0100

    création fichier constantes.h

---
+++ src/constantes.h
@@ -0,0 +1,10 @@
+#ifndef DEF_CONSTANTES
+#define DEF_CONSTANTES
+
+    #define TAILLE_BLOC         32 // Taille d'un bloc (carré) en pixels
+    #define NB_BLOCS_LARGEUR    13
+    #define NB_BLOCS_HAUTEUR    26
+    #define LARGEUR_FENETRE     TAILLE_BLOC * NB_BLOCS_LARGEUR
+    #define HAUTEUR_FENETRE     TAILLE_BLOC * NB_BLOCS_HAUTEUR
+
+#endif
