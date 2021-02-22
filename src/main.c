0e9dc05f5a7a23427137baaf16231aa48be35b0f
Arthur Scherrer <arthurscherrer72@gmail.com>
2021-02-22 17:39:46 +0100

    comm

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <SDL.h>
-#include <SDL/SDL_image.h>
 #include <SDL/SDL_ttf.h>
 
 
@@ -13,30 +12,16 @@
 
 
 
-#define W_WINDOW 1000
-#define H_WINDOW 500
 
 
-// gcc src/main.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image
-
+// gcc src/main.c -o bin/prog -I include -L lib -lmingw32 -lSDL2main -lSDL2 
 
 
 
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
-
->>>>>>> 1405c1f0fe71171d5cc1dc74392e8acabcd7fa3f
 int main(int argc, char *argv[])
 {
 
-    creer_menu ();
+    creer_menu (  );
    
     return EXIT_SUCCESS;
 
