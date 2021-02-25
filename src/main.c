3d44411cf1edd895f8e10f97c38f1b0352c71dcd
Lucas Bouillon <lucasbouillon72@gmail.com>
2021-02-25 09:19:41 +0100

    réindentation

--- src/main.c
+++ src/main.c
@@ -32,6 +32,7 @@ int main(int argc, char *argv[])
         SDL_Rect surface_rect;
         Mix_Music * musique = NULL;
         Mix_Music * clic_settings = NULL;
+        SDL_Event event;
         SDL_bool curseur;
 
 
@@ -138,140 +139,138 @@ int main(int argc, char *argv[])
         while(program_launched) // boucle infinie qui va attendre les évènements 
         {
 
-        SDL_Event event;
 
-
-        while(SDL_PollEvent(&event))
-        {
-                curseur = SDL_FALSE;
-
-                switch(event.type)
+                while(SDL_PollEvent(&event))
                 {
+                        curseur = SDL_FALSE;
 
-                        case SDL_MOUSEBUTTONDOWN:
-                                
-                                // on cherche à savoir si les coordonnées du clic se trouvent dans les coordonnées de la texture
-                                if ( (((event.button.x >= (W_WINDOW - 80)) && ((event.button.x) <= (W_WINDOW - 80) + 32)) && (((event.button.y) >= 15) && ((event.button.y) <= 15 + 32))) )
-                                {
+                        switch(event.type)
+                        {
 
+                                case SDL_MOUSEBUTTONDOWN:
                                         
-
-                                        if ( Mix_PausedMusic() == 1 ) //Si la musique est en pause
+                                        // on cherche à savoir si les coordonnées du clic se trouvent dans les coordonnées de la texture
+                                        if ( (((event.button.x >= (W_WINDOW - 80)) && ((event.button.x) <= (W_WINDOW - 80) + 32)) && (((event.button.y) >= 15) && ((event.button.y) <= 15 + 32))) )
                                         {
 
-                                        Mix_ResumeMusic(); //Reprendre la musique
+                                                
 
-                                        icon_son.chaine = "img/volume_on.bmp"; // charge l'image du volume "ON"
+                                                if ( Mix_PausedMusic() == 1 ) //Si la musique est en pause
+                                                {
 
-                                        SDL_RenderClear(renderer); 
+                                                Mix_ResumeMusic(); //Reprendre la musique
 
-                                        creer_image(background);
-                                        creer_image(icon_son);
+                                                icon_son.chaine = "img/volume_on.bmp"; // charge l'image du volume "ON"
 
-                                        SDL_RenderPresent(renderer);
-                                        
+                                                SDL_RenderClear(renderer); 
+
+                                                creer_image(background);
+                                                creer_image(icon_son);
+
+                                                SDL_RenderPresent(renderer);
+                                                
+                                                }
+
+                                                else
+                                                {
+
+                                                Mix_PauseMusic(); //Mettre en pause la musique
+
+                                                icon_son.chaine = "img/volume_off.bmp"; // charge l'image du volume "OFF"
+
+                                                SDL_RenderClear(renderer);
+
+                                                creer_image(background);
+                                                creer_image(icon_son);
+
+                                                SDL_RenderPresent(renderer);
+                                                
+                                                }
+
                                         }
 
+                                        if ( (((event.button.x >= (774 - (189/2))) && ((event.button.x) <= (774 + (189/2)))) && (((event.button.y) >= 487 - (59/2)) && ((event.button.y) <= 487 + (59/2)))) )
+                                        {
+
+                                                //jouer();
+                                
+
+                                        }
+
+                                        else if ( (((event.button.x >= (813 - (347/2))) && ((event.button.x) <= (813 + (347/2)))) && (((event.button.y) >= 599 - (73/2)) && ((event.button.y) <= 599 + (73/2)))) )
+                                        {
+                                                clic_settings = Mix_LoadMUS("sounds/coin.wav"); //Chargement de la musique
+                                                Mix_PlayMusic(clic_settings, 1); //Jouer infiniment la musique
+                                                settings(window, renderer);
+                                        }
+
+                                        break;
+
+
+
+
+
+                                case SDL_MOUSEMOTION:
+
+                                        // centre play x            : 774
+                                        // centre play y            : 487
+                                        // long play                : 189
+                                        // haut play                : 59
+
+
+                                        // centre settings x        : 813
+                                        // centre settings y        : 599
+                                        // long setting             : 347
+                                        // haut setting             : 73
+
+                                        if ( (((event.motion.x >= (774 - (189/2))) && ((event.motion.x) <= (774 + (189/2)))) && (((event.motion.y) >= 487 - (59/2)) && ((event.motion.y) <= 487 + (59/2)))) )
+                                        {
+
+                                                //Curseur sur l'onglet "play"
+                                                curseur = SDL_TRUE;
+
+                                                //Coordonnées du rectangle "play"
+                                                surface_rect.x = 774 - (189/2);
+                                                surface_rect.y = 487 - (59/2);
+                                                surface_rect.w = 220;
+                                                surface_rect.h = 60;
+                                
+
+                                        }
+
+                                        else if ( (((event.motion.x >= (813 - (347/2))) && ((event.motion.x) <= (813 + (347/2)))) && (((event.motion.y) >= 599 - (73/2)) && ((event.motion.y) <= 599 + (73/2)))) )
+                                        {
+                                                //Curseur sur l'onglet "settings"
+                                                curseur = SDL_TRUE; 
+
+                                                //Coordonnées du rectangle "settings"
+                                                surface_rect.x = 813 - (360/2);
+                                                surface_rect.y = 599 - (73/2);
+                                                surface_rect.w = 320;
+                                                surface_rect.h = 70;
+                                        }
                                         else
                                         {
 
-                                        Mix_PauseMusic(); //Mettre en pause la musique
+                                                //Aucune selection
+                                                curseur = SDL_FALSE; 
 
-                                        icon_son.chaine = "img/volume_off.bmp"; // charge l'image du volume "OFF"
-
-                                        SDL_RenderClear(renderer);
-
-                                        creer_image(background);
-                                        creer_image(icon_son);
-
-                                        SDL_RenderPresent(renderer);
-                                        
                                         }
 
-                                }
-
-                                if ( (((event.button.x >= (774 - (189/2))) && ((event.button.x) <= (774 + (189/2)))) && (((event.button.y) >= 487 - (59/2)) && ((event.button.y) <= 487 + (59/2)))) )
-                                {
-
-                                        //jouer();
-                        
-
-                                }
-
-                                else if ( (((event.button.x >= (813 - (347/2))) && ((event.button.x) <= (813 + (347/2)))) && (((event.button.y) >= 599 - (73/2)) && ((event.button.y) <= 599 + (73/2)))) )
-                                {
-                                        clic_settings = Mix_LoadMUS("sounds/coin.wav"); //Chargement de la musique
-                                        Mix_PlayMusic(clic_settings, 1); //Jouer infiniment la musique
-                                        settings(window, renderer);
-                                }
-
-                                break;
+                                        break;
 
 
 
+                                
+                                case SDL_QUIT:
 
+                                        program_launched = SDL_FALSE;
 
-                        case SDL_MOUSEMOTION:
-
-                                // centre play x            : 774
-                                // centre play y            : 487
-                                // long play                : 189
-                                // haut play                : 59
-
-
-                                // centre settings x        : 813
-                                // centre settings y        : 599
-                                // long setting             : 347
-                                // haut setting             : 73
-
-                                if ( (((event.motion.x >= (774 - (189/2))) && ((event.motion.x) <= (774 + (189/2)))) && (((event.motion.y) >= 487 - (59/2)) && ((event.motion.y) <= 487 + (59/2)))) )
-                                {
-
-                                        //Curseur sur l'onglet "play"
-                                        curseur = SDL_TRUE;
-
-                                        //Coordonnées du rectangle "play"
-                                        surface_rect.x = 774 - (189/2);
-                                        surface_rect.y = 487 - (59/2);
-                                        surface_rect.w = 220;
-                                        surface_rect.h = 60;
-                        
-
-                                }
-
-                                else if ( (((event.motion.x >= (813 - (347/2))) && ((event.motion.x) <= (813 + (347/2)))) && (((event.motion.y) >= 599 - (73/2)) && ((event.motion.y) <= 599 + (73/2)))) )
-                                {
-                                        //Curseur sur l'onglet "settings"
-                                        curseur = SDL_TRUE; 
-
-                                        //Coordonnées du rectangle "settings"
-                                        surface_rect.x = 813 - (360/2);
-                                        surface_rect.y = 599 - (73/2);
-                                        surface_rect.w = 320;
-                                        surface_rect.h = 70;
-                                }
-                                else
-                                {
-
-                                        //Aucune selection
-                                        curseur = SDL_FALSE; 
-
-                                }
-
-                                break;
-
-
-
-                        
-                        case SDL_QUIT:
-
-                                program_launched = SDL_FALSE;
-
-                                break;
-                        
-                        default:
-                                break;
-                        
+                                        break;
+                                
+                                default:
+                                        break;
+                                
                         }
 
                         if(curseur == SDL_TRUE){ //Affiche un encadrement blanc autour de la selection
